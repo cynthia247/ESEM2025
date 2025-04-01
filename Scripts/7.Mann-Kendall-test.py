3f0d241e5f87c04cabb7d4e75684d74e07354bd5
cynthia247 <shamse.cynthia@usask.ca>
2025-04-01 16:09:26 -0600

    updating script

--- Scripts/7.Mann-Kendall-test.py
+++ Scripts/7.Mann-Kendall-test.py
@@ -1,15 +1,21 @@
 import pymannkendall as mk
 import pandas as pd
+import os
 
 # Example data (Replace with your actual data)
-df = pd.read_csv('cs+satd-datasets/tesseract_cs+satd.csv')
-community_smells = df['OS']
-satd_counts = df['SATD']
+directoy = 'Project-types/small'
 
-# Mann-Kendall Test for Community Smells
-result_smells = mk.original_test(community_smells)
-print("Community Smells Trend:", result_smells)
+for dirpath, dirnames, filenames in os.walk(directoy):
+    for filename in filenames:
+        print("\nFile:", filename)
+        df = pd.read_csv(os.path.join(dirpath, filename))
+        community_smells = df['OS']
+        satd_counts = df['SATD']
+            
+        # Mann-Kendall Test for Community Smells
+        result_smells = mk.original_test(community_smells)
+        print("Community Smells Trend:", result_smells)
 
-# Mann-Kendall Test for SATD
-result_satd = mk.original_test(satd_counts)
-print("SATD Trend:", result_satd)
+        # Mann-Kendall Test for SATD
+        result_satd = mk.original_test(satd_counts)
+        print("SATD Trend:", result_satd)
